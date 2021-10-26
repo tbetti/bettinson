b151387df6213d0dd16932114a3ee69d8bb02610
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-10-26 15:40:29 -0400

    Set up app and index files inside the src folder

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import Components from './components/...';
+import Header from './components/js/Header';
+
+export default function App(){
+    return(
+        <div>
+            <Header />
+        </div>
+    )
+}
