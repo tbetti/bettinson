0e5aedc7938f6fe579ab7ae0ed8faaee0b825ce4
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-10-26 17:00:23 -0400

    Added About Me page

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import Components from './components/...';
 import Header from './components/js/Header';
 import Footer from './components/js/Footer';
+import AboutMe from './components/js/pages/AboutMe';
 
 export default function App(){
+    // const [clicked, setClicked] = useState(false);
+    // clicked={clicked} setClicked={setClicked}
     return(
         <div>
             <Header />
+            <AboutMe />
             <Footer />
         </div>
     )
---
+++ src/components/js/pages/AboutMe.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function AboutMe() {
+    return (
+        <div className="about-section" id="about-me">
+            <h2>Hi, I'm Tiana!</h2>
+            <div className="section-text">
+                <div className="about-img-container">
+                    <img className="about-img" src="/components/images/Me1.jpg" alt="Tiana Bettinson"/>
+                </div>
+                <p>
+                    A former elementary school math teacher turned full-stack web developer.
+                    <span> I utilize my experience leading in the classroom to manage large projects/groups by breaking down
+                        complex concepts and delegating tasks.</span>
+                    Currently enrolled in Columbia University’s Coding Bootcamp!
+                </p>
+                <p>Check out my work!</p>
+            </div>
+
+        </div>
+    )
+}
