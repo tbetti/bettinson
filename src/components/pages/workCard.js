8f62cf6e0fa662c4d9b1defe302be4fd3732a9af
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-11-01 16:40:05 -0400

    Adjusted styles for work card overlays

--- src/components/pages/workCard.js
+++ src/components/pages/workCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {displayInfo, stopProp} from '../js/util';
+import { displayInfo, stopProp } from '../js/util';
 
 function workCard(props) {
     return (
@@ -8,9 +8,11 @@ function workCard(props) {
             <img src={props.image} alt={props.description} name={props.id} />
             {/* Add overlay that displays title, description, and technologies used when image clicked */}
             <div className="overlay" id={props.id}>
-                <h3>{props.title}</h3>
-                <p>{props.description}</p>
-                <p>{props.type}</p>
+                <div className="text-box" id="text-box">
+                    <h3>{props.title}</h3>
+                    <p>{props.description}</p>
+                    <p>{props.type}</p>
+                </div>
                 {/* Add buttons to link to websites and GitHub repositories */}
                 <div className="btn-box">
                     <button className="card-btn" onClick={stopProp}>
